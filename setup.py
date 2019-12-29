a253742402f1f6a716ca4478c25b697ffcffb7d2
Swapnil Patel <sp70215@gmail.com>
2019-12-29 01:03:21 -0800

    Create setup file for package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="usc-schedule",
+    version="0.0.1",
+    author="Swapnil Patel",
+    author_email="swapnil@usc.edu",
+    description="A USC Class Schedule API wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TrickRoom/USC-schedule",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
