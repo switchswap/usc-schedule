05cc98fdf66ed87ff46e709fb85e7985be58a716
Swapnil Patel <sp70215@gmail.com>
2019-12-29 01:32:12 -0800

    Fix setup.py
    Fixed classifier for license and added dependencies

--- setup.py
+++ setup.py
@@ -13,9 +13,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TrickRoom/USC-schedule",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'json'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
