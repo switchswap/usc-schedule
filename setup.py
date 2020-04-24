ab81021e274bab3c87f9c738653a0844301cec20
Swapnil Patel <sp70215@gmail.com>
2020-04-24 16:34:29 -0700

    Update setup.py url field with my new username

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A USC Class Schedule API wrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TrickRoom/USC-schedule",
+    url="https://github.com/switchswap/USC-schedule",
     packages=setuptools.find_packages(),
     install_requires=[
         'requests'
