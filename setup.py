9af9225f9bf90a9a4e14f34f4c1bab14849b67a0
Swapnil Patel <sp70215@gmail.com>
2020-05-02 00:33:29 -0700

    Update version number
    Updated to version 1.0.0! 
    I would consider this the first official release since I'm happy with all the features implemented and it's no longer a 1-to-1 of the API.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="usc-schedule",
-    version="0.1.1",
+    version="1.0.0",
     author="Swapnil Patel",
     author_email="swapnil@usc.edu",
     description="A USC Class Schedule API wrapper",
