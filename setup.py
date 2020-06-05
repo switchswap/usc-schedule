3f7d8e9a742ca718a634d1112ec888f115b31515
Swap <sp70215@gmail.com>
2020-06-05 00:41:46 -0700

    Update version number
    Updated version to 1.0.1!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="usc-schedule",
-    version="1.0.0",
+    version="1.0.1",
     author="Swapnil Patel",
     author_email="swapnil@usc.edu",
     description="A USC Class Schedule API wrapper",
